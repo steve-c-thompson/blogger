3f738efb28a2c4943daf7dfce3cd3c9515252536
Steve Thompson <sthompson@SDInstructor1.local>
2018-09-27 14:48:27 -0600

    Fix bug in spec - was using Article.new instead of Article.create!() .

--- spec/features/user_deletes_article_from_show_spec.rb
+++ spec/features/user_deletes_article_from_show_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 describe 'user deletes article' do
   describe 'they link from the show page' do
     it 'displays all articles without the deleted entry' do
-      article_1 = Article.new(title: 'New title 1', body: "New body 1")
-      article_2 = Article.new(title: 'New title 2', body: "New body 2")
-
-
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_2.title)
+      article_1 = Article.create(title: 'New title 1', body: "New body 1")
+      article_2 = Article.create(title: 'New title 2', body: "New body 2")
 
       visit article_path(article_1)
 
