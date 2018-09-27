a42eb9dcaf8ba5cd77171e645887da2dd55f45c4
Steve Thompson <sthompson@SDInstructor1.local>
2018-09-27 14:41:43 -0600

    Add test for delete that visits articles page and clicks commit. Not sure why we don't look in the Article object as well.

---
+++ spec/features/user_deletes_article_from_show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'user deletes article' do
+  describe 'they link from the show page' do
+    it 'displays all articles without the deleted entry' do
+      article_1 = Article.new(title: 'New title 1', body: "New body 1")
+      article_2 = Article.new(title: 'New title 2', body: "New body 2")
+
+
+      expect(page).to have_content(article_1.title)
+      expect(page).to have_content(article_2.title)
+
+      visit article_path(article_1)
+
+      click_link "Delete"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content(article_2.title)
+      expect(page).to_not have_content(article_1.title)
+
+    end
+  end
+end
