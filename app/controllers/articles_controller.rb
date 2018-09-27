bf8f2d8f63a64434f29c0d884ab3da34d354b023
Steve Thompson <sthompson@SDInstructor1.local>
2018-09-27 11:47:22 -0600

    Add spec for individual article with tests.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,10 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    #binding.pry
+    #byebug
+    @article = Article.find(params[:id])
+  end
 end
