286f792dbdace8d12734601b4280bab7cff3511a
Steve Thompson <sthompson@SDInstructor1.local>
2018-09-28 13:05:22 -0600

    Redo migration for Tags and Taggings to include timestamps.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928184007) do
+ActiveRecord::Schema.define(version: 20180928190420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,16 @@ ActiveRecord::Schema.define(version: 20180928184007) do
   create_table "taggings", force: :cascade do |t|
     t.bigint "tag_id"
     t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_taggings_on_article_id"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
