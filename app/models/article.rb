476efd2b04b4c292acc9c11e6060135a9ebd7c9c
Steve Thompson <sthompson@SDInstructor1.local>
2018-09-28 12:45:59 -0600

    Add relationships between tags, taggings, articles, and going through taggings.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   validates_presence_of :title, :body
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe Tagging, type: :model do
   describe "relationships" do
     it do
-      should belong_to(:tags)
+      should belong_to(:tag)
     end
     it do
-      should belong_to(:articles)
+      should belong_to(:article)
     end
   end
 end
