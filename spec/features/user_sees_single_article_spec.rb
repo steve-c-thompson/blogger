b9f7db94f8c37513bc9aeb2f81a737a187a5032c
Steve Thompson <sthompson@SDInstructor1.local>
2018-09-28 10:31:22 -0600

    Add comment spec for view, and functionality.

--- spec/features/user_sees_single_article_spec.rb
+++ spec/features/user_sees_single_article_spec.rb
@@ -4,12 +4,19 @@ describe "user sees one article" do
   describe 'they link from the articles index' do
     it 'displays article data' do
       article_1 = Article.create!(title: "New Article", body: "New Body")
+      comment_1 = article_1.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article_1.comments.create(author_name: "You", body: "So much to say")
+
       visit articles_path
 
       click_link article_1.title
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 
